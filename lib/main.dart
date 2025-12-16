1b98c4aa1c539068a9a421e2b398b4c4575dffc7
mihiranwijesekara <achinthamihiran654@gmail.com>
2025-12-16 23:10:18 +0530

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:chicken_dilivery/pages/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart'; // add
-import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+// import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'dart:io';
 import 'database/database_helper.dart'; // ADD
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  if (!kIsWeb && (Platform.isWindows || Platform.isLinux)) {
-    sqfliteFfiInit();
-    databaseFactory = databaseFactoryFfi;
-  }
+  // if (!kIsWeb && (Platform.isWindows || Platform.isLinux)) {
+  //   sqfliteFfiInit();
+  //   databaseFactory = databaseFactoryFfi;
+  // }
 
   if (!kIsWeb) {
     final db = await DatabaseHelper.instance.database;
